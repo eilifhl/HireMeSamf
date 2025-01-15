247ec7284c5ce455737e9ae9b096cdd955140662
Eilif Hjermann Lindblad <eilif.hjermann@gmail.com>
2025-01-15 14:47:46 +0100

    Change font to helvetica.

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Hire me, Samf'; /* The text */
+var txt = 'HIRE ME, SAMF'; /* The text */
 var speed = 20; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
