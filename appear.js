081895e225929af41b57f76e390e1e00daf5ccc9
Eilif Hjermann Lindblad <eilif.hjermann@gmail.com>
2025-01-16 14:43:04 +0100

    Add button for cat.

--- appear.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-        const image = document.getElementById('fadeImage');
-        const sound = document.getElementById('fadeSound');
-        image.style.opacity = 1;
-        sound.play();
-    }, 3000); // 3000 milliseconds = 3 seconds
-};
